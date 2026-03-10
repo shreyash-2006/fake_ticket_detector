668d5565f3edfa98f32cff0010f4455b79253c76
shreyash-2006 <shreyashbhor2006@gmail.com>
2026-03-10 12:09:01 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,23 @@
+import sqlite3
 
+
+conn = sqlite3.connect('railway_updatede.db')
+cursor = conn.cursor()
+
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS passengers (
+        ticket_id TEXT PRIMARY KEY,
+        source TEXT,
+        destination TEXT,
+        passenger_quantity integer,
+        booked_on date,
+        expiry_date date,
+        class integer,
+        ticket_price float,
+        verified bool     
+    )'''
+)
+conn.commit()
+conn.close()
+print("Database created")
